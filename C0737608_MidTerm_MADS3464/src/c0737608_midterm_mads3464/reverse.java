57840e5b16b509069dfc511689bbc64cefcc23c9
MacStudent <macstudent@20X-iMacXX-M-394.local>
2018-06-07 15:31:28 -0400

    Reverse String

---
+++ C0737608_MidTerm_MADS3464/src/c0737608_midterm_mads3464/reverse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0737608_midterm_mads3464;
+
+/**
+ *
+ * @author macstudent
+ */
+public class reverse {
+    
+    public static void main(String[] args) {
+
+    String input = "Lambton";
+    char[] Str = input.toCharArray();
+    
+    for(int i=Str.length-1; i>=0; i--){
+        System.out.println(Str[i]);
+        
+    
+    }
+    }    
+    
+}
